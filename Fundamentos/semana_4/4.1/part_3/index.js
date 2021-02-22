e353b58c923d8911338d4e033f5097c71c283429
Emerson Saturnino <emersonsaturnino@pop-os.localdomain>
2021-02-22 16:15:10 -0300

    Exercicio proposto semana 4 parte 3 conteúdo

---
+++ Fundamentos/semana_4/4.1/part_3/index.js
@@ -0,0 +1,9 @@
+const base = 5;
+const altura = 8;
+area = base * altura;
+
+console.log('---- Área de um retângulo ----');
+console.log('Base: '+base);
+console.log('Altura: '+altura);
+console.log('Área: '+area);
+console.log('---- Fim ----');
