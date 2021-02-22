8a2494b439d1f57d9a0e5fc59e617633b355c407
Emerson Saturnino <emersonsaturnino@pop-os.localdomain>
2021-02-22 16:05:20 -0300

    Exercicio proposto semana 4 parte 2 conteúdo

---
+++ Fundamentos/semana_4/4.1/part_2/index.js
@@ -0,0 +1,19 @@
+const name = "Emerson Luis Saturnino";
+const birthCity = "Vargem Grande do Sul SP";
+let birthYear = 1984;
+
+console.log('-------');
+console.log(name);
+console.log(birthCity);
+console.log(birthYear);
+console.log('-------');
+birthYear = 2020;
+console.log(birthYear);
+console.log('-------');
+
+//birthCity = "Teste";
+
+//Altere o valor atribuído a constante birthCity . 
+//Faça um console.log(birthCity) novamente! Você saberia explicar porque recebemos uma mensagem de erro?
+
+//Erro pois foi tentado atribuir um valor a uma constante e isso não é possível.
